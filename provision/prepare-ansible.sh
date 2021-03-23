76eef5c42a6fe76f66f1b097448ba1be005082ae
Rodrigo Vieira Geromin <rodrigo.geromin@tecnogroup.com.br>
2021-03-23 10:26:58 -0300

    Corrigindo projeto para nova versão da collection do k8s

--- provision/prepare-ansible.sh
+++ provision/prepare-ansible.sh
@@ -5,5 +5,7 @@ sudo chown vagrant. kubespray -R
 cd kubespray
 cp -a inventory/sample inventory/bexs/
 cp -a /home/vagrant/files/inventory.ini inventory/bexs/
-sudo pip3 install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install -r requirements.txt
 
